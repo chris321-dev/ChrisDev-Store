74faa43965f12d8b4b53ba4e8479fdcbf63785af
Christian Okafor <okaforchristian08@gmail.com>
2024-02-21 19:34:52 +0100

    Update cart.jsx

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -27,7 +27,7 @@ export const Cart = () => {
       {totalAmount > 0 ? (
         <div className="checkout">
           <h3> Subtotal: ${totalAmount} </h3>
-          <button onClick={() => navigate("/")}> <h3>Continue Shopping</h3> </button>
+          <button onClick={() => navigate("/ChrisDev-Store")}> <h3>Continue Shopping</h3> </button>
           <button
             onClick={() => {
               checkout();
