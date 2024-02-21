6f95d9eb6fddbf74f3903718417ef0c7729eeca7
Christian Okafor <okaforchristian08@gmail.com>
2024-02-21 18:26:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
+            <Route path="/ChrisDev-Store" element={<Shop />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
