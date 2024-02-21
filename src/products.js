a6898693a03c8d723ba0a1f2627953e9e1841a9f
Christian Okafor <okaforchristian08@gmail.com>
2024-02-21 18:00:00 +0100

    first commit

---
+++ src/products.js
@@ -0,0 +1,80 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+import product9 from "./assets/products/9.jpg";
+import product10 from "./assets/products/10.jpg";
+import product11 from "./assets/products/11.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "YT Boot",
+    price: 899.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Gucci Watch",
+    price: 2311.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Smart TV",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "HP Intel",
+    price: 728.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Infinix 9",
+    price: 328.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Ps4 GamePad",
+    price: 90.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Z Team Bag",
+    price: 70.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Thermal Printer",
+    price: 120.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "PS5 Game",
+    price: 228.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "Denim Jacket",
+    price: 68.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: " Vejaro Top",
+    price: 28.0,
+    productImage: product11,
+  },
+];
